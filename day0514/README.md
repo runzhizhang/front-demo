cd33c293c036bc9a2f8a48e9d4f972cf3306fbcd
zhangrunzhi <3116555436@qq.com>
2021-05-14 16:22:51 +0800

    changeInfo first

---
+++ day0514/README.md
@@ -0,0 +1,2 @@
+1. ul下的li水平排列：设置li的display：inline-block
+2. 注意：服务端返回的字符串形式的额json数据中的字符串，必须使用双引号，否则 使用JSON.parse方法进行转换时会出错
---
+++ day0514/changeInfo.js
@@ -0,0 +1,58 @@
+const frontURL = 'https://01d148a4-abf0-4775-8d16-680a2f8282f6.mock.pstmn.io/font'
+const backURL = 'https://01d148a4-abf0-4775-8d16-680a2f8282f6.mock.pstmn.io/back'
+let frontData = null
+let backData = null
+$(function () {
+  // 数据请求
+  $('.header li')[0].className = 'sec'
+  $.ajax({
+    url: frontURL,
+    success: function (data) {
+      frontData = JSON.parse(data).list
+      render(frontData)
+      // 添加事件
+      $('.header li').eq(0).click(function () {
+        this.className = 'sec'
+        $('.header li')[1].className = ''
+        render(frontData)
+        console.log('front btn')
+      })
+      $('.header li').eq(1).click(function () {
+        this.className = 'sec'
+        $('.header li')[0].className = ''
+        if (backData) {
+          render(backData)
+          console.log('back btn')
+        }
+      })
+      $('.listContent').eq(0).mouseover(function () {
+        $('.tips')[0].style.display = 'inline-block'
+        console.log('over')
+      })
+      $('.listContent').eq(0).mouseout(function () {
+        $('.tips')[0].style.display = 'none'
+        console.log('out')
+      })
+    }
+  })
+  $.ajax({
+    url: backURL,
+    success: function (data) {
+      backData = JSON.parse(data).list
+    }
+  })
+  function render (dataList) { //data数组形式的数据
+    let listContent = $('.listContent')[0]
+    if (dataList) {
+      // 页面渲染之前清空
+      listContent.innerHTML = ''
+      // 页面渲染
+      for (let i = 0; i < dataList.length; i++) {
+        let node = document.createElement('li')
+        let context = document.createTextNode(dataList[i].title)
+        node.appendChild(context)
+        listContent.appendChild(node)
+      }
+    }
+  }
+})
